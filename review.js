65d849c1eab3100ba33ae939249ee7aff3b9468c
Joe Heasly <jheasly1@maine.rr.com>
2020-09-01 11:09:48 -0400

    deconstructing Notes on usage

--- review.js
+++ review.js
@@ -12,22 +12,11 @@ hobbies.push('Home Renovation');
 
 console.log(hobbies);
 
-// spread operator ... (three dot prefix) - copy array or object - deep copy, not new reference to orig array/object
-// cf. slice operator, which returns a 'slice' of an array (which may be the entire array)
+// deconstructing - like spread in that array ( [] ) or object ( {} ) context must be provided
+//                - different in that you specify the members you want
+//                - ojbect deconstruction - use property names
+//                - array deconstruction - use position
 
-// spread with array context
-const newHobbies = [...hobbies];
-console.log(newHobbies);
-
-// spread with object context
-const newPerson = {...person};
-console.log(person);
-console.log(newPerson);
-
-// spread in 'cross' contexts
-// const arrayPerson = [...person];
-// console.log(arrayPerson);  // error: TypeError: person is not iterable
-
-const objectHobbies = {...hobbies};
-console.log(objectHobbies);  // no error, but array elements are assigned numeric property names:
-                             //  { '0': 'Sports', '1': 'Singing', '2': 'Home Renovation' }
+const [, index1, index2] = hobbies; // this will get unwieldy with large arrays!
+                                    // expecting 'Singing, Home Renovation'
+console.log(index1, index2);
