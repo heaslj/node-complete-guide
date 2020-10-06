8b4c1ab595302a70c7b8cd507f794624520fdd9c
Joe Heasly <jheasly1@maine.rr.com>
2020-10-06 10:53:04 -0400

    mongodb-1 Updates I made following along...

--- models/product.js
+++ models/product.js
@@ -1,27 +1,73 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const getDb = require('../util/database').getDb;
 
-const sequelize = require('../util/database');
-
-const Product = sequelize.define('product', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  title: Sequelize.STRING,
-  price: {
-    type: Sequelize.DOUBLE,
-    allowNull: false
-  },
-  imageUrl: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  description: {
-    type: Sequelize.STRING,
-    allowNull: false
+class Product {
+  constructor(title, price, description, imageUrl, id, userId) {
+    this.title = title;
+    this.price = price;
+    this.description = description;
+    this.imageUrl = imageUrl;
+    this._id = id ? new mongodb.ObjectId(id) : null;
+    this.userId = userId;
   }
-});
+
+  save() {
+    const db = getDb();
+    let saveOperation;
+    if(this._id) {
+      // product exists, so update
+      saveOperation = db.collection('products')
+      .updateOne( {_id: this._id}, {$set: this});
+    } else {
+      saveOperation = db.collection('products')
+      .insertOne(this)
+    }
+    return saveOperation
+    .then( result => {
+      console.log(result);
+    })
+    .catch( err => {
+      console.log(err);
+    });
+  }
+
+  static fetchAll() {
+    const db = getDb();
+    return db.collection('products')
+      .find().toArray()
+      .then(products => {
+        console.log(products);
+        return products;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  static findById(prodId) {
+    const db = getDb();
+    return db.collection('products')
+      .findOne( {_id: new mongodb.ObjectId(prodId)} )
+      .then(product => {
+        console.log(product);
+        return product;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  static deleteById(prodId) {
+    const db = getDb();
+    return db.collection('products')
+    .deleteOne( { _id: new mongodb.ObjectId(prodId)} )
+    .then(result => {
+      console.log('Deleted product: ', prodId);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  }
+}
 
 module.exports = Product;
--- util/database.js
+++ util/database.js
@@ -1,8 +1,26 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'ivCXu588vNCK1pUsumfs65ZQ', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+let _db;
 
-module.exports = sequelize;
+const mongoConnect = (callback) => {
+  MongoClient.connect('mongodb+srv://node-user:7TIFCEwXaOnBgOf0@completenodejs-cluster0.wdl2t.mongodb.net/shop?retryWrites=true&w=majority')
+  .then(client => {
+    console.log('Connected');
+    _db = client.db();
+    callback();
+  })
+  .catch(err => {
+    console.log('Connect failed', err)
+  });
+};
+
+const getDb = () => {
+  if(_db) {
+    return _db;
+  }
+  throw 'Database not found.'
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
