8919913b5154104a8d25d9ec9d95ee164dc3f132
Joe Heasly <jheasly1@maine.rr.com>
2020-10-27 11:13:51 -0400

    authentication-4 Updates following along with presentation

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -1,5 +1,6 @@
 module.exports = (req, res, next) => {
     if (!req.session.isLoggedIn) {
+        req.flash('error', 'Please login first.');
         return res.redirect('/login');
     }
     next();
